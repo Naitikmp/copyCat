2ccba4ff6f74d47964f2ff3ed8d73a2773867212
vizzv <vishwa10032003@gmail.com>
2022-05-11 17:03:31 +0530

    copCat :meow

---
+++ config.py
@@ -0,0 +1,19 @@
+##OPEN API STUFF
+OPENAI_API_KEY = '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'
+
+
+
+## FLASK STUFF
+class Config(object):
+    DEBUG = True
+    TESTING = False
+
+class DevelopmentConfig(Config):
+    SECRET_KEY = "this-is-a-super-secret-key"
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': DevelopmentConfig,
+    'production': DevelopmentConfig
+}
